3968283cea69537ee74759acafd0c76ffa357522
Anastasia Charushina <stasia-a@tut.by>
2012-09-27 11:31:54 +0300

    Initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AssetPipeline::Application.config.secret_token = '7e4cbb03fd3936e1785f5842e07b2454469481228c270c68a352ac019c59aaebad5335e0ae04648c6e6ac82123974301c5e76ad872a6795da8c424160178c37f'
